25e33024ed1b5c079936b36120423f1777395903
xiaobao8 <1362994036@qq.com>
2019-04-03 17:02:20 -0500

    删除index

--- index.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('this is first page')
-
-
-def login(request):
-    return HttpResponse('this is first login page')
